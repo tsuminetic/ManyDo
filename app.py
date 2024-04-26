96143ce50c714e21cc2371d65819193351d1b400
tsuminetic <diensanc@gmail.com>
2024-04-26 11:48:45 +0545

    small changes

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    @login_manager.user_loader
+    @login_manager.user_loader  
     def load_user(id: str) -> User:
         return User.query.get(int(id))
 
